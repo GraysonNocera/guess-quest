4248dd12ec15eca6f74778cd728c6549ce8317e4
Grayson Nocera <gnocera@purdue.edu>
2025-05-30 11:24:43 -0400

    Initial Commit

---
+++ HeadsUp/HeadsUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeadsUpApp.swift
+//  HeadsUp
+//
+//  Created by Grayson Nocera on 5/30/25.
+//
+
+import SwiftUI
+
+@main
+struct HeadsUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
